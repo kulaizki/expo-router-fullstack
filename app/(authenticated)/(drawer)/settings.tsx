dbad76e73094e9f494ee23d7c9af7124cfb1b723
kulaizki <kulaizke@gmail.com>
2024-08-07 21:48:01 +0800

    feat: add onToggle for drawer

--- app/(authenticated)/(drawer)/settings.tsx
+++ app/(authenticated)/(drawer)/settings.tsx
@@ -1,10 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
+import { useNavigation, useRouter } from 'expo-router';
 import React from 'react'
+import { DrawerActions } from '@react-navigation/native';
 
 const Page = () => {
+
+  const navigation = useNavigation();
+  const router = useRouter();
+
+  const onToggle = () => {
+    navigation.dispatch(DrawerActions.toggleDrawer())
+  }
+
   return (
     <View>
       <Text>Settings</Text>
+      <Button title="Toggle Drawer" onPress={onToggle} />
     </View>
   )
 }
