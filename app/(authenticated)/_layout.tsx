fde5cc8c0593a83bbeb5b0709ff6b1dc89df1737
kulaizki <kulaizke@gmail.com>
2024-08-08 14:25:54 +0800

    feat: add details/[id] layout

--- app/(authenticated)/_layout.tsx
+++ app/(authenticated)/_layout.tsx
@@ -1,12 +1,12 @@
-import { Stack } from 'expo-router'
+import { Stack } from "expo-router";
 
 const Layout = () => {
   return (
     <Stack>
-      <Stack.Screen name='(drawer)' options={{ headerShown: false }} />
-      <Stack.Screen name='details/[id]' />
+      <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
+      <Stack.Screen name="details/[id]" />
     </Stack>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
