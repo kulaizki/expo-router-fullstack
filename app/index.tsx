37d7d101316191e2503937a291b3f34d9d31ca04
kulaizki <kulaizke@gmail.com>
2024-08-08 15:02:03 +0800

    feat: add login-page ui & hooks

--- app/index.tsx
+++ app/index.tsx
@@ -1,46 +1,146 @@
-import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
-import { Colors } from "@/constants/Colors";
-import { Link } from "expo-router";
+import {
+	View,
+	Image,
+	StyleSheet,
+	TextInput,
+	ActivityIndicator,
+	TouchableOpacity,
+	Text
+} from 'react-native';
+import React, { useState } from 'react';
+import { useAuth } from '@/context/AuthContext';
+import { Link } from 'expo-router';
+import { Colors } from '@/constants/Colors';
 
-const Page = () => {
-  return (
-    <View style={styles.container}>
-      <Image
-        source={{ uri: "https://galaxies.dev/img/logos/logo--blue.png" }}
-        style={styles.image}
-      />
-      <Link href='/(authenticated)/(drawer)/(tabs)/home' asChild>
-        <TouchableOpacity>
-          <Text style={{ color: "#fff" }}>Login</Text>
+const Login = () => {
+	const [email, setEmail] = useState('simon@galaxies.dev');
+	const [password, setPassword] = useState('123456');
+	const [loading, setLoading] = useState(false);
+
+	const { onLogin } = useAuth();
+
+	const login = async () => {
+		setLoading(true);
+
+		const result = await onLogin!(email, password);
+
+		if (result && result.error) {
+			alert(result.msg);
+		}
+		setLoading(false);
+	};
+
+	return (
+		<View style={styles.container}>
+			<Image
+				source={{ uri: 'https://galaxies.dev/img/logos/logo--blue.png' }}
+				style={styles.image}
+			/>
+			<Text style={styles.header}>Galaxies</Text>
+			<Text style={styles.subheader}>The app to be.</Text>
+
+			<TextInput
+				autoCapitalize="none"
+				placeholder="john@doe.com"
+				value={email}
+				onChangeText={setEmail}
+				style={styles.inputField}
+				placeholderTextColor={'#fff'}
+			/>
+			<TextInput
+				placeholder="password"
+				value={password}
+				onChangeText={setPassword}
+				secureTextEntry
+				style={styles.inputField}
+				placeholderTextColor={'#fff'}
+			/>
+			{/* <Link href={'/(authenticated)/(drawer)/(tabs)/home'} asChild replace>
+        <TouchableOpacity style={styles.button}>
+          <Text style={{ color: '#fff' }}>Sign in</Text>
         </TouchableOpacity>
-      </Link>
-      <Link href="/register" asChild>
-        <TouchableOpacity>
-          <Text style={{ color: "#fff" }}>Register</Text>
-        </TouchableOpacity>
-      </Link>
-      <Link href="/privacy" asChild>
-        <TouchableOpacity>
-          <Text style={{ color: "#fff" }}>Privacy</Text>
-        </TouchableOpacity>
-      </Link>
-    </View>
-  );
+      </Link> */}
+
+			<TouchableOpacity onPress={login} style={styles.button}>
+				<Text style={{ color: '#fff' }}>Sign in</Text>
+			</TouchableOpacity>
+
+			<Link href={'/register'} asChild>
+				<TouchableOpacity style={styles.outlineButton}>
+					<Text style={{ color: '#fff' }}>Create Account</Text>
+				</TouchableOpacity>
+			</Link>
+
+			<Link href={'/privacy'} asChild>
+				<TouchableOpacity style={{ alignItems: 'center' }}>
+					<Text style={{ color: Colors.primary }}>Privacy Policy</Text>
+				</TouchableOpacity>
+			</Link>
+
+			{loading && (
+				<View
+					style={[
+						StyleSheet.absoluteFill,
+						{ backgroundColor: 'rgba(0,0,0,0.6)', zIndex: 1, justifyContent: 'center' }
+					]}
+				>
+					<ActivityIndicator color="#fff" size="large" />
+				</View>
+			)}
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: Colors.background,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  image: {
-    width: "100%",
-    height: 100,
-    resizeMode: "contain",
-  },
+	container: {
+		flex: 1,
+		padding: 20,
+		backgroundColor: Colors.background,
+		justifyContent: 'center'
+	},
+	image: {
+		width: '100%',
+		height: 100,
+		resizeMode: 'contain'
+	},
+	header: {
+		fontSize: 40,
+		textAlign: 'center',
+		marginBottom: 10,
+		color: '#fff'
+	},
+	subheader: {
+		fontSize: 18,
+		textAlign: 'center',
+		marginBottom: 40,
+		color: '#fff'
+	},
+	inputField: {
+		marginVertical: 4,
+		height: 50,
+		borderWidth: 1,
+		borderColor: Colors.primary,
+		borderRadius: 4,
+		padding: 10,
+		color: '#fff',
+		backgroundColor: Colors.input
+	},
+	button: {
+		marginTop: 20,
+		alignItems: 'center',
+		backgroundColor: Colors.primary,
+		padding: 12,
+		borderRadius: 4
+	},
+	outlineButton: {
+		marginVertical: 8,
+		alignItems: 'center',
+		backgroundColor: 'transparent',
+		padding: 12,
+		borderRadius: 4,
+		borderWidth: 1,
+		borderColor: Colors.primary
+	}
 });
 
-export default Page;
+export default Login;
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,5 +1,5 @@
 export const Colors = {
   background: '#171630',
-  input: '1b1b32',
-  primary: '582c96',
+  input: '#1b1b32',
+  primary: '#582c96',
 }
